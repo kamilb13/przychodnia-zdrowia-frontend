d78610d91c66c7b1e4a70c94724b36fc36d0b89c
kamilb13 <kamil.brzoza13@gmail.com>
2025-04-02 10:28:15 +0200

    register patient page

--- src/app/pages/register-patient/register-patient.component.ts
+++ src/app/pages/register-patient/register-patient.component.ts
@@ -1,8 +1,10 @@
 import {Component, inject} from '@angular/core';
 import {MatStep, MatStepLabel, MatStepper, MatStepperNext, MatStepperPrevious} from '@angular/material/stepper';
-import {MatFormField, MatInput, MatLabel} from '@angular/material/input';
+import {MatFormField, MatInput, MatInputModule, MatLabel} from '@angular/material/input';
 import {FormBuilder, ReactiveFormsModule, Validators} from '@angular/forms';
 import {MatButton} from '@angular/material/button';
+import {NgIf} from '@angular/common';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @Component({
   selector: 'app-register-patient',
@@ -17,6 +19,9 @@ import {MatButton} from '@angular/material/button';
     MatStepperPrevious,
     MatStepperNext,
     MatStepLabel,
+    NgIf,
+    MatFormFieldModule,
+    MatInputModule,
   ],
   templateUrl: './register-patient.component.html',
   styleUrl: './register-patient.component.scss'
@@ -26,9 +31,16 @@ export class RegisterPatientComponent {
 
   firstFormGroup = this._formBuilder.group({
     firstCtrl: ['', Validators.required],
+    surnameCtrl: ['', Validators.required],
   });
   secondFormGroup = this._formBuilder.group({
-    secondCtrl: ['', Validators.required],
+    ssnCtrl: ['', [Validators.minLength(11), Validators.maxLength(11), Validators.required, Validators.pattern('^[0-9]*$')]],
   });
   isLinear = true;
+  protected readonly alert = alert;
+
+  addPatient() {
+    //TODO add patient logic
+    console.log("Pacjent dodany do systemu.");
+  }
 }
