9b2888220cfc8b3eb11bd62768735a50ef74c3f0
Mikołaj Pacierz <mikolajpacierz18@gmail.com>
2025-04-17 12:05:28 +0200

    changed JsonResponse

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 import {HomeComponent} from './pages/home/home.component';
 import {DashboardComponent} from './layout/dashboard/dashboard.component';
-import {VisitsComponent} from './pages/visits/visits.component';
+import {AddVisitComponent} from './pages/add-visit/add-visit.component';
 import {RegisterPatientComponent} from './pages/register-patient/register-patient.component';
 import {AllPatientsComponent} from './pages/all-patients/all-patients.component';
 import {AddDoctorComponent} from './pages/add-doctor/add-doctor.component';
@@ -10,7 +10,7 @@ import {AllDoctorsComponent} from './pages/all-doctors/all-doctors.component';
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
-  { path: 'visits', component: VisitsComponent },
+  { path: 'visits/new', component: AddVisitComponent },
   { path: 'patients', component: AllPatientsComponent },
   { path: 'patients/new', component: RegisterPatientComponent },
   { path: 'doctors', component: AllDoctorsComponent },
---
+++ src/app/pages/add-visit/add-visit.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-visits',
+  imports: [],
+  templateUrl: './add-visit.component.html',
+  styleUrl: './add-visit.component.scss'
+})
+export class AddVisitComponent {
+
+}
