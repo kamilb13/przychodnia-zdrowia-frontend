1a450c0847e41b1acb48479aac1750feffa07d6c
kamilb13 <kamil.brzoza13@gmail.com>
2025-05-08 20:10:33 +0200

    delete doctor functionality

--- src/app/pages/all-doctors/all-doctors.component.ts
+++ src/app/pages/all-doctors/all-doctors.component.ts
@@ -11,9 +11,11 @@ import {
     MatHeaderRowDef,
     MatRow, MatRowDef, MatTable, MatTableDataSource
 } from '@angular/material/table';
+import {MatButton} from "@angular/material/button";
+import {MatSnackBar} from "@angular/material/snack-bar";
 
 @Component({
-  selector: 'app-all-doctors',
+    selector: 'app-all-doctors',
     imports: [
         MatCell,
         MatCellDef,
@@ -25,36 +27,56 @@ import {
         MatRow,
         MatRowDef,
         MatTable,
-        MatHeaderCellDef
+        MatHeaderCellDef,
+        MatButton,
     ],
-  templateUrl: './all-doctors.component.html',
-  styleUrl: './all-doctors.component.scss'
+    templateUrl: './all-doctors.component.html',
+    styleUrl: './all-doctors.component.scss'
 })
 
 export class AllDoctorsComponent implements OnInit {
-  private http = inject(HttpClient);
-  patients: any[] = [];
-  displayedColumns: string[] = ['id', 'name', 'surname'];
-  dataSource = new MatTableDataSource<any>([]);
+    private snackBar = inject(MatSnackBar);
+    private http = inject(HttpClient);
+    patients: any[] = [];
+    displayedColumns: string[] = ['id', 'name', 'surname', 'actions'];
+    dataSource = new MatTableDataSource<any>([]);
 
-  @ViewChild(MatPaginator) paginator!: MatPaginator;
+    @ViewChild(MatPaginator) paginator!: MatPaginator;
 
-  ngOnInit() {
-    this.getAllDoctors()
-  }
+    ngOnInit() {
+        this.getAllDoctors()
+    }
 
-  getAllDoctors() {
-    this.http.get<any[]>('http://localhost:8080/doctors').subscribe({
-      next: (response) => {
-        console.log('Doktorzy:', response);
-        this.patients = response;
-        this.dataSource.data = response;
-        this.dataSource.paginator = this.paginator;
-      },
-      error: (error) => {
-        console.error('Błąd pobierania doktorów:', error);
-      }
-    });
-  }
+    getAllDoctors() {
+        this.http.get<any[]>('http://localhost:8080/doctors').subscribe({
+            next: (response) => {
+                console.log('Doktorzy:', response);
+                this.patients = response;
+                this.dataSource.data = response;
+                this.dataSource.paginator = this.paginator;
+            },
+            error: (error) => {
+                console.error('Błąd pobierania doktorów:', error);
+            }
+        });
+    }
+
+    deleteDoctor(doctorId: number) {
+        this.http.delete(`http://localhost:8080/doctors/${doctorId}`).subscribe({
+            next: (response) => {
+                console.log(response)
+                this.snackBar.open('Doktor został pomyślnie usunięty', 'Zamknij', {
+                    duration: 3000,
+                });
+                // this.patients = this.patients.filter((patient) => patient.id !== doctorId);
+                this.getAllDoctors();
+            },
+            error: (error)=> {
+                this.snackBar.open('Wystąpił błąd przy usuwaniu doktora', 'Zamknij', {
+                    duration: 4000,
+                });
+            }
+        })
+    }
 }
 
